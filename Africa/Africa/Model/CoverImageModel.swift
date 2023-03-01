6909cedd414471db20815d35f93376e1bc162149
Amr Muhammad <amrmu94@gmail.com>
2023-03-01 22:19:12 +0200

    add  coverImageView and parse json data

---
+++ Africa/Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImage Model.swift
+//  Africa
+//
+//  Created by Amr Muhammad on 01/03/2023.
+//
+
+import Foundation
+
+struct CoverImage:Codable,Identifiable{
+    let id:Int
+    let name:String
+}
